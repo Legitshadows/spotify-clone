9bc1ee647e15788501fdfa6ad08d9439cb29d7ae
Legitshadows <la.beltran19@info.uas.edu.mx>
2023-11-11 15:08:54 -0700

    added box and a new install

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import path from "path";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -31,8 +32,23 @@ const Sidebar: React.FC<SidebarProps> = ({
     ], [pathname]);
 
     return (
-        <div>
-            Sidebar Test!
+        <div className="flex h-full">
+            <div
+            className="hidden
+            md:flex
+            flex-col
+            gap-y-2
+            bg-black
+            h-full
+            w-[300px]
+            p-2
+            "
+            >
+                <Box>
+                    Sidebar Navigation
+                </Box>
+
+            </div>
         </div>
     );
 }
