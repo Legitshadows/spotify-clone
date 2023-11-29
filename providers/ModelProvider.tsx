234bb44ce417f8a66de3f1f2958536ac775661ae
Legitshadows <la.beltran19@info.uas.edu.mx>
2023-11-29 11:48:55 -0700

    added modelprovider and added it to layout

---
+++ providers/ModelProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const ModelProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+          Models Test
+        </>
+    );
+}
+
+export default ModelProvider;
